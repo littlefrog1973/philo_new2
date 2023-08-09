ee4fa24b71137c8705d5338225aba2472565e304
Sukit Deeyiengyang <kitdee@hotmail.com>
2023-08-09 23:06:35 +0700

    revised time stamp

--- philo/src/pthread.c
+++ philo/src/pthread.c
@@ -6,7 +6,7 @@
 /*   By: sdeeyien <sukitd@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 08:13:14 by sdeeyien          #+#    #+#             */
-/*   Updated: 2023/08/08 14:40:44 by sdeeyien         ###   ########.fr       */
+/*   Updated: 2023/08/09 14:14:29 by sdeeyien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,10 +33,12 @@ void *philo_odd(void *args)
 	t_philo	*ptr_philo;
 	int	n_philo;
 	int	state;
+	unsigned long	s_time;
 
 	state = STATE_IDLE;
 	ptr_philo = (t_philo *) args;
-	ptr_philo->s_time = ptr_philo->lm_time = current_time();
+	s_time = ptr_philo->lm_time = current_time();
+//	ptr_philo->s_time = ptr_philo->lm_time = current_time();
 	id = ptr_philo->id;
 	n_eat = ptr_philo->philo_prop.n_eat;
 	n_philo = ptr_philo->philo_prop.n_philo;
@@ -45,30 +47,30 @@ void *philo_odd(void *args)
 		if (is_die(ptr_philo))
 		{
 			state = STATE_DEAD;
-			prt_state(current_time(), id, &state);
+			prt_state(current_time() - s_time, id, &state);
 			break;
 		}
-		prt_state(current_time(), id, &state);
+		prt_state(current_time() - s_time, id, &state);
 //		usleep(STEP);
 		if (ptr_philo->forks[id - 1] == 0 && !is_die(ptr_philo))
 		{
 			pthread_mutex_lock(&(ptr_philo->ptr_mutex_forks[id - 1]));
 			ptr_philo->forks[id - 1] = id;
-			printf("%lu %d has taken a forks\n", current_time(), id);
+			printf("%lu %d has taken a forks\n", current_time() - s_time, id);
 			if (ptr_philo->forks[id % n_philo] == 0 && !is_die(ptr_philo))
 			{
 				pthread_mutex_lock(&(ptr_philo->ptr_mutex_forks[id % n_philo]));
 				ptr_philo->forks[id % n_philo] = id;
-				printf("%lu %d has taken a forks\n", current_time(), id);
+				printf("%lu %d has taken a forks\n", current_time() - s_time, id);
 				ptr_philo->lm_time = current_time();
-				printf("%lu %d is eating\n", current_time(), id);
+				printf("%lu %d is eating\n", current_time() - s_time, id);
 				usleep(ptr_philo->philo_prop.t_eat * 1000);
 				n_eat--;
 				ptr_philo->forks[id % n_philo] = 0;
 				pthread_mutex_unlock(&(ptr_philo->ptr_mutex_forks[id % n_philo]));
 				ptr_philo->forks[id - 1] = 0;
 				pthread_mutex_unlock(&(ptr_philo->ptr_mutex_forks[id - 1]));
-				printf("%lu %d is sleeping\n", current_time(), id);
+				printf("%lu %d is sleeping\n", current_time() - s_time, id);
 				state = STATE_IDLE;
 				usleep(ptr_philo->philo_prop.t_sleep * 1000);
 			}
@@ -81,16 +83,16 @@ void *philo_odd(void *args)
 			{
 				pthread_mutex_lock(&(ptr_philo->ptr_mutex_forks[id % n_philo]));
 				ptr_philo->forks[id % n_philo] = id;
-				printf("%lu %d has taken a forks\n", current_time(), id);
+				printf("%lu %d has taken a forks\n", current_time() - s_time, id);
 				ptr_philo->lm_time = current_time();
-				printf("%lu %d is eating\n", current_time(), id);
+				printf("%lu %d is eating\n", current_time()- s_time, id);
 				usleep(ptr_philo->philo_prop.t_eat * 1000);
 				n_eat--;
 				ptr_philo->forks[id % n_philo] = 0;
 				pthread_mutex_unlock(&(ptr_philo->ptr_mutex_forks[id % n_philo]));
 				ptr_philo->forks[id - 1] = 0;
 				pthread_mutex_unlock(&(ptr_philo->ptr_mutex_forks[id - 1]));
-				printf("%lu %d is sleeping\n", current_time(), id);
+				printf("%lu %d is sleeping\n", current_time() - s_time, id);
 				state = STATE_IDLE;
 				usleep(ptr_philo->philo_prop.t_sleep * 1000);
 			}
@@ -124,10 +126,12 @@ void *philo_even(void *args)
 	t_philo	*ptr_philo;
 	int		n_philo;
 	int	state;
+	unsigned long	s_time;
 
 	state = STATE_IDLE;
 	ptr_philo = (t_philo *) args;
-	ptr_philo->s_time = ptr_philo->lm_time = current_time();
+	s_time = ptr_philo->lm_time = current_time();
+//	ptr_philo->s_time = ptr_philo->lm_time = current_time();
 	id = ptr_philo->id;
 	n_eat = ptr_philo->philo_prop.n_eat;
 	n_philo = ptr_philo->philo_prop.n_philo;
@@ -136,30 +140,30 @@ void *philo_even(void *args)
 		if (is_die(ptr_philo))
 		{
 			state = STATE_DEAD;
-			prt_state(current_time(), id, &state);
+			prt_state(current_time() - s_time, id, &state);
 			break;
 		}
-		prt_state(current_time(), id, &state);
+		prt_state(current_time() - s_time, id, &state);
 //		usleep(STEP);
 		if (ptr_philo->forks[id % n_philo] == 0 && !is_die(ptr_philo))
 		{
 			pthread_mutex_lock(&(ptr_philo->ptr_mutex_forks[id % n_philo]));
 			ptr_philo->forks[id % n_philo] = id;
-			printf("%lu %d has taken a forks\n", current_time(), id);
+			printf("%lu %d has taken a forks\n", current_time() - s_time, id);
 			if (ptr_philo->forks[id - 1] == 0 && !is_die(ptr_philo))
 			{
 				pthread_mutex_lock(&(ptr_philo->ptr_mutex_forks[id - 1]));
 				ptr_philo->forks[id - 1] = id;
-				printf("%lu %d has taken a forks\n", current_time(), id);
+				printf("%lu %d has taken a forks\n", current_time() - s_time, id);
 				ptr_philo->lm_time = current_time();
-				printf("%lu %d is eating\n", current_time(), id);
+				printf("%lu %d is eating\n", current_time() - s_time, id);
 				usleep(ptr_philo->philo_prop.t_eat * 1000);
 				n_eat--;
 				ptr_philo->forks[id - 1] = 0;
 				pthread_mutex_unlock(&(ptr_philo->ptr_mutex_forks[id - 1]));
 				ptr_philo->forks[id % n_philo] = 0;
 				pthread_mutex_unlock(&(ptr_philo->ptr_mutex_forks[id % n_philo]));
-				printf("%lu %d is sleeping\n", current_time(), id);
+				printf("%lu %d is sleeping\n", current_time() - s_time, id);
 				state = STATE_IDLE;
 				usleep(ptr_philo->philo_prop.t_sleep * 1000);
 			}
@@ -172,9 +176,9 @@ void *philo_even(void *args)
 			{
 				pthread_mutex_lock(&(ptr_philo->ptr_mutex_forks[id - 1]));
 				ptr_philo->forks[id - 1] = id;
-				printf("%lu %d has taken a forks\n", current_time(), id);
+				printf("%lu %d has taken a forks\n", current_time() - s_time, id);
 				ptr_philo->lm_time = current_time();
-				printf("%lu %d is eating\n", current_time(), id);
+				printf("%lu %d is eating\n", current_time() - s_time, id);
 //				state = STATE_IDLE;
 				usleep(ptr_philo->philo_prop.t_eat * 1000);
 				n_eat--;
@@ -182,7 +186,7 @@ void *philo_even(void *args)
 				pthread_mutex_unlock(&(ptr_philo->ptr_mutex_forks[id - 1]));
 				ptr_philo->forks[id % n_philo] = 0;
 				pthread_mutex_unlock(&(ptr_philo->ptr_mutex_forks[id % n_philo]));
-				printf("%lu %d is sleeping\n", current_time(), id);
+				printf("%lu %d is sleeping\n", current_time() - s_time, id);
 				state = STATE_IDLE;
 				usleep(ptr_philo->philo_prop.t_sleep * 1000);
 			}
